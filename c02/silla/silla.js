4e7fdc72d05b68f1fea67e34151e9a8e725f40c5
nsgs0815 <141768833+nsgs0815@users.noreply.github.com>
2023-09-19 14:16:44 -0500

    Add files via upload

--- c02/silla/silla.js
+++ c02/silla/silla.js
@@ -1,49 +1,61 @@
 const scene = new THREE.Scene();
-camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
+camera = new THREE.PerspectiveCamera(40, window.innerWidth / window.innerHeight, 0.1, 1000);
 const renderer = new THREE.WebGLRenderer();
 renderer.setSize(window.innerWidth, window.innerHeight);
 document.body.appendChild(renderer.domElement);
 
-function cubo(ancho, alto, profundo){
+//medidas del cubo
+const ancho = 0.8; const alto = ancho; const profundo = 0.2;
+//medidas del cilindro
+const radioSuperior = 0.4; const radioInferior = radioSuperior; const grosor = 0.2
+
+function cubo() {
     const cubo = new THREE.BoxGeometry(ancho, alto, profundo); //constructor de geometria de cubo cuyos valores son(Float: ancho, Float: alto, Float: profundo)
     const material = new THREE.MeshNormalMaterial(); //mesh o textura del cubo
     const cube = new THREE.Mesh(cubo, material); //objeto cubo
     scene.add(cube);
+    return cube;
 }
 
-cubo(2,2,2);
+function cilindro() {
+    const cilindro = new THREE.CylinderGeometry(radioSuperior, radioInferior, grosor, 32); //constructor de geometria de cilindro cuyos valores son(Float: radioSuperior, Float: radioInferior, Float: altura, Integer: segmentosRadiales)
+    const material2 = new THREE.MeshNormalMaterial(); //mesh o textura del cilindro
+    const cylinder = new THREE.Mesh(cilindro, material2); //objeto cilindro
+    scene.add(cylinder);
+    cylinder.rotation.x = 1.57;
+    return cylinder;
+}
 
-//cilindro
-const cilindro = new THREE.CylinderGeometry(1, 1, 1, 32); //constructor de geometria de cilindro cuyos valores son(Float: radioSuperior, Float: radioInferior, Float: altura, Integer: segmentosRadiales)
-const material2 = new THREE.MeshNormalMaterial(); //mesh o textura del cilindro
-const cylinder = new THREE.Mesh( cilindro, material2 ); //objeto cilindro
-scene.add( cylinder );
-cylinder.position.y = cube.position.y + 2;
-//cylinder.rotation.y = 90;
-cylinder.rotation.x = 1.57;
 
-camera.position.z = 9;
-renderer.render(scene, camera);
+const asiento = new cubo();
+const espaldar = new cilindro();
+espaldar.position.y = asiento.position.y + ancho;
+
+
+
 
 function animate() {
     requestAnimationFrame(animate);
     cube.rotation.x += 0.01;
     cube.rotation.y += 0.01;
-    
-    cylinder.rotation.x +=0.01;
-    cylinder.rotation.y +=0.01;
+
+    cylinder.rotation.x += 0.01;
+    cylinder.rotation.y += 0.01;
     renderer.render(scene, camera);
 }
 
-function ortogonal(){ 
-    camera = new THREE.OrthographicCamera(-10,10,5,-5, 0.01, 1000);
+function ortogonal() {
+    camera = new THREE.OrthographicCamera(-10, 10, 5, -5, 0.01, 1000);
     camera.position.z = 10;
     renderer.render(scene, camera);
 }
 
-function perspectiva(){
+function perspectiva() {
     camera = new THREE.PerspectiveCamera(50, window.innerWidth / window.innerHeight, 0.1, 1000);
     camera.position.z = 9;
     renderer.render(scene, camera);
 }
+
+camera.position.z = 9;
+renderer.render(scene, camera);
 //animate();
