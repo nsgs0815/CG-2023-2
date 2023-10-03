18de340f27da70896f3f9da19a7b00b2fbd6e526
nsgs0815 <141768833+nsgs0815@users.noreply.github.com>
2023-10-03 14:34:16 -0500

    Add files via upload

--- c02/tubo/tubo.js
+++ c02/tubo/tubo.js
@@ -9,29 +9,24 @@ camera.position.z = 8; //posicion de la camara en Z
 
 const puntoCurva = new THREE.CatmullRomCurve3([//puntos que definen la curva que dibuja el tubo, vectores (x,y,z)
     new THREE.Vector3(0, 0, 0),   // Punto de inicio
-    new THREE.Vector3(0, 2, 0),   // Punto intermedio en la parte superior
-    new THREE.Vector3(2, 2, 0),   // Punto en la parte superior derecha
-    new THREE.Vector3(2, 0, 0),   // Punto en la parte derecha
-    new THREE.Vector3(0, 0, 0),   // Punto en la parte inferior derecha
-    new THREE.Vector3(0, -2, 0),  // Punto intermedio en la parte inferior
-    new THREE.Vector3(2, -2, 0),  // Punto en la parte inferior derecha
-    new THREE.Vector3(2, 0, 0),
+    new THREE.Vector3(0, 2, 0),   // Punto en la parte superior de la pata izquierda
+    new THREE.Vector3(2, 2, 0),   // Punto en la parte superior de la pata derecha
+    new THREE.Vector3(2, 0, 0),   // Punto en la parte inferior de la pata derecha
 ]);
 
 const points = puntoCurva.getPoints(50); //optiene los puntos del arreglo de vectores "puntoCurva"
-
 const curva = new THREE.CatmullRomCurve3(points) //curva generada con el contructor dados los puntos optenidos anteriormente
-const geometry = new THREE.TubeGeometry(curva, 64, 0.6, 32, false); //constructor para geometria de tubo(linea o curva, segmentos tubulares,)
-const material = new THREE.MeshPhongMaterial();
-const tubeMesh = new THREE.Mesh(geometry, material);
-scene.add(tubeMesh);
+const geometry = new THREE.TubeGeometry(curva, 64, 0.6, 32, false); //constructor para geometria de tubo(linea o curva, segmentos tubulares,radio,segmentos radiales,cerrado o abierto)
+const material = new THREE.MeshPhongMaterial(); //material para usaer luces y sombras
+const tubeMesh = new THREE.Mesh(geometry, material); //objeto malla que genera el tubo
+scene.add(tubeMesh); //añade el tubo a la escena
 
-var hemiLight = new THREE.HemisphereLight(0x000000, 0xBC94F9, 1);
-hemiLight.position.set(0, 500, 0);
+var hemiLight = new THREE.HemisphereLight(0x000000, 0xBC94F9, 1); //constructor para luz hemisferica(color del cielo, color del suelo, intensidad de luz)
+hemiLight.position.set(15, 500, 15); //posicionb de la luz en (x,y,z)
 scene.add(hemiLight)
 
 
-renderer.render(scene, camera);
+renderer.render(scene, camera); //carga (renderiza) la escena y la camara
 function ortogonal() {//permite alternar a camara ortogonal
     camera = new THREE.OrthographicCamera(-10, 10, 5, -5, 0.01, 1000);
     camera.position.z = 10;
@@ -43,7 +38,7 @@ function perspectiva() {//permite cambiar a camara perspectiva
     camera.position.z = 9;
     renderer.render(scene, camera);
 }
-const animate = function () {
+function animate() {
     requestAnimationFrame(animate);
 
     tubeMesh.rotation.x += 0.01;
@@ -52,4 +47,4 @@ const animate = function () {
     renderer.render(scene, camera);
 };
 
-//animate();
+animate();
